d29c035f7946635f2065149ae92cdeb45e158ef6
HerndonE <EHerndon17@gmail.com>
2020-09-30 23:24:26 -0700

    [add] basic waypoint script

---
+++ Unity/CSUMBRacingGame/Assets/EthanH/Scripts/CheckPointSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointSystem : MonoBehaviour
+{
+
+    public GameObject playerNCar;
+   // public int size;
+    public GameObject[] waypointObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //waypointObjects = new GameObject[size];
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //WayPointFunction();   
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Wow Ethan!");
+        //for(int i = 0; i < waypointObjects.Length; i++)
+        //{
+        //Debug.Log(i);
+        if (playerNCar.gameObject == waypointObjects[0])
+            {
+                Debug.Log("Do Something");
+            }
+        //}
+    }
+
+}
