e66b32be2ecffdf790312f857c549d9b78d23448
HerndonE <EHerndon17@gmail.com>
2020-10-23 00:22:41 -0700

    add cars and npc bus

---
+++ Unity/CSUMBRacingGame/Assets/EthanH/Scripts/spawnVehicles.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Ethan Herndon
+ * 10/23/2020
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnVehicles : MonoBehaviour
+{
+    public GameObject otterExpress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(otterExpress, new Vector3(-347.3f, 2.3f, -142.69f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
